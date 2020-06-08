9a503f8b9202825116f973e495cd42ba830fa4f1
Gergo1212 <gergo.meszaros.codecool@gmail.com>
2020-06-08 13:22:55 +0200

    theme created, Colors.js. added

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -5,29 +5,35 @@ import PokemonList from "./components/PokemonList";
 import Pokemon from "./components/Pokemon";
 import Types from './components/Types';
 import Header from "./components/Header";
+import ThemeContext from "./ThemeContext";
 
 function App() {
+    const theme = 'light';
+
+
     return (
-        <div className="App">
-        <Router>
-            <Route path='/'>
-                <div>This is the header</div>
-                <Header/>
-            </Route>
-            <Route path='/pokemons'>
-                <div>This is the pokemons route!!</div>
-                <PokemonList/>
-            </Route>
-            <Route path='/types'>
-                <div>Pokemon Type</div>
-                <Types/>
-            </Route>
-            <Route path={'/pokemon/:id'}>
-                <div>Pokemon with id</div>
-                <Pokemon/>
-            </Route>
-        </Router>
-        </div>
+        <ThemeContext.Provider value={theme}>
+            <div className="App">
+                <Router>
+                    <Route path='/'>
+                        <div>This is the header</div>
+                        <Header/>
+                    </Route>
+                    <Route path='/pokemons'>
+                        <div>This is the pokemons route!!</div>
+                        <PokemonList/>
+                    </Route>
+                    <Route path='/types'>
+                        <div>Pokemon Type</div>
+                        <Types/>
+                    </Route>
+                    <Route path={'/pokemon/:id'}>
+                        <div>Pokemon with id</div>
+                        <Pokemon/>
+                    </Route>
+                </Router>
+            </div>
+        </ThemeContext.Provider>
     );
 }
 
