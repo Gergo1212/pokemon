902d92247e8e58216c82543f312100e69adaf0b0
Gergo1212 <gergo.meszaros.codecool@gmail.com>
2020-06-08 13:29:58 +0200

    ThemeToggler.js added

--- pokemon/src/components/Header.js
+++ pokemon/src/components/Header.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 
-function Header (){
+function Header() {
+    const headerStyles = {
+        padding: "1rem",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center"
+    }
 
-        return (
-            <div>
-                <Link to={'/pokemons'}>Pokemons</Link> <br/>
-                <Link to={'/types'}>Types</Link> <br/>
-                <Link to={'/'}>Main Page</Link>
-            </div>
-        );
+    return (
+        <div>
+            <header style={headerStyles}>
+                <h1>Context API</h1>
+                <ThemeToggler/>
+            </header>
+            <Link to={'/pokemons'}>Pokemons</Link> <br/>
+            <Link to={'/types'}>Types</Link> <br/>
+            <Link to={'/'}>Main Page</Link>
+        </div>
+    );
 }
 
 export default Header;
